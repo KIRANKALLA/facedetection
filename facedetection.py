12d159934abe9385e68ad9fd6ca1d06276b61faa
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 16:46:41 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -7,9 +7,8 @@ from PIL import Image
 import numpy as np
 
 img_file_buffer=st.camera_input('Take a pic')
-if img_file_buffer is not None:
-    img=Image.open(img_file_buffer)
-    img_array=np.array(img)
+img=Image.open(img_file_buffer)
+img_array=np.array(img)
 
 image = imutils.resize(img_array,width=400)
 prototxt = 'deploy.prototxt'
