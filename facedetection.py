b623b2030d946a453a4d8412a203a9f9e09e61dd
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 18:56:03 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -6,16 +6,21 @@ import streamlit as st
 from PIL import Image
 import numpy as np
 
-img_file_buffer=st.camera_input('Take a pic')
-img=Image.open(img_file_buffer)
-img_array=np.array(img)
+picture=st.camera_input('Take a picture')
+if picture is not None:
+	img=Image.open(picture)
+	img_array=np.array(img)
 
-image = imutils.resize(img_array,width=400)
+image = imutils.resize(img_array, width=400)
 (h, w) = image.shape[:2]
+
 prototxt = 'deploy.prototxt'
 model = 'res10_300x300_ssd_iter_140000.caffemodel'
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
+
+#image = imutils.resize(image, width=400)
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
 net.setInput(blob)
 detections = net.forward()
 
@@ -27,6 +32,7 @@ for i in range(0, detections.shape[2]):
 	# filter out weak detections by ensuring the `confidence` is
 	# greater than the minimum confidence threshold
 	if confidence > 0.5:
+		print(confidence)
 		# compute the (x, y)-coordinates of the bounding box for the object
 		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
@@ -35,5 +41,5 @@ for i in range(0, detections.shape[2]):
 		y = startY - 10 if startY - 10 > 10 else startY + 10
 		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 		cv2.putText(image, text, (startX, y),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+		cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 st.image(image)
