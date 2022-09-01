aaf4593761e463aeb5d360a1591feaf0713e7bdb
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 15:57:00 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -1,9 +1,6 @@
 import imutils
 import numpy as np
 import cv2
-from google.colab.patches import cv2_imshow
-from IPython.display import display, Javascript
-from google.colab.output import eval_js
 from base64 import b64decode
 import streamlit as st
 from PIL import Image
