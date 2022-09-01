71fe300db199ded498f400f47d125d676bb1fd1f
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 16:50:58 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -27,13 +27,12 @@ for i in range(0, detections.shape[2]):
 	# greater than the minimum confidence threshold
 	if confidence > 0.5:
 		# compute the (x, y)-coordinates of the bounding box for the object
-		box = detections[0, 0, i, 3:7] 
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
 		(startX, startY, endX, endY) = box.astype("int")
 		# draw the bounding box of the face along with the associated probability
 		text = "{:.2f}%".format(confidence * 100)
 		y = startY - 10 if startY - 10 > 10 else startY + 10
 		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 		cv2.putText(image, text, (startX, y),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 st.image(image)
