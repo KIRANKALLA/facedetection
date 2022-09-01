208701a6aa45eb38e78587a287265c75820aea1a
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 06:25:30 +0530

    Add files via upload

---
+++ facedetection.py
@@ -0,0 +1,40 @@
+import cv2
+import imutils
+import numpy as np
+from PIL import Image
+import streamlit as st
+
+image = st.camera_input("Take a picture")
+
+if image is not None:
+    bytes_data = image.getvalue()
+    
+
+image=np.array(image)
+
+prototxt = 'deploy.prototxt'
+model = 'res10_300x300_ssd_iter_140000.caffemodel'
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
+image = imutils.resize(image, width=400)
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+
+net.setInput(blob)
+detections = net.forward()
+for i in range(0, detections.shape[2]):
+
+	# extract the confidence (i.e., probability) associated with the prediction
+	confidence = detections[0, 0, i, 2]
+
+	# filter out weak detections by ensuring the `confidence` is
+	# greater than the minimum confidence threshold
+	if confidence > 0.5:
+		# compute the (x, y)-coordinates of the bounding box for the object
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+		# draw the bounding box of the face along with the associated probability
+		text = "{:.2f}%".format(confidence * 100)
+		y = startY - 10 if startY - 10 > 10 else startY + 10
+		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
+		cv2.putText(image, text, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+st.image(image)
