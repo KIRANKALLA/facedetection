9dbd1476e9e627809ecc7488b56380c685e2fe60
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 16:28:10 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -10,22 +10,21 @@ img_file_buffer=st.camera_input('Take a pic')
 if img_file_buffer is not None:
     img=Image.open(img_file_buffer)
     img_array=np.array(img)
-
-#img=Image.open(img_file_buffer)
-#img_array=np.array(img)
     image = imutils.resize(img_array, width=400)
     (h, w) = image.shape[:2]
 
-    prototxt = 'deploy.prototxt'
-    model = 'res10_300x300_ssd_iter_140000.caffemodel'
-    net = cv2.dnn.readNetFromCaffe(prototxt, model)
-    blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
-    net.setInput(blob)
-    detections = net.forward()
+prototxt = 'deploy.prototxt'
+model = 'res10_300x300_ssd_iter_140000.caffemodel'
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
+net.setInput(blob)
+detections = net.forward()
 
-    for i in range(0, detections.shape[2]):
+for i in range(0, detections.shape[2]):
+
 	# extract the confidence (i.e., probability) associated with the prediction
 	confidence = detections[0, 0, i, 2]
+
 	# filter out weak detections by ensuring the `confidence` is
 	# greater than the minimum confidence threshold
 	if confidence > 0.5:
@@ -37,7 +36,8 @@ if img_file_buffer is not None:
 		y = startY - 10 if startY - 10 > 10 else startY + 10
 		cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
 		cv2.putText(image, text, (startX, y),
-		cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+			cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
 
 
-    st.image(image)
+st.image(image)
+
