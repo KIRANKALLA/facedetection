5f6e5440c17f21ce397fd2cb2b5eea54b70caa18
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 16:06:26 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -7,7 +7,6 @@ from PIL import Image
 import numpy as np
 
 img_file_buffer=st.camera_input('Take a pic')
-img_array= np.array()
 if img_file_buffer is not None:
     img=Image.open(img_file_buffer)
     img_array=np.array(img)
