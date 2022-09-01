7087b9a43d6c69346a5e2f60ff0f2b928f07d5fa
KIRANKALLA <kallakiran1974@gmail.com>
2022-09-01 10:30:24 +0530

    Update facedetection.py

--- facedetection.py
+++ facedetection.py
@@ -10,12 +10,12 @@ if image is not None:
     bytes_data = image.getvalue()
     
 
-image=np.array(image)
+#image=np.array(image)
 
 prototxt = 'deploy.prototxt'
 model = 'res10_300x300_ssd_iter_140000.caffemodel'
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
-#image = imutils.resize(image, width=400)
+image = imutils.resize(image, width=400)
 blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
 
 net.setInput(blob)
